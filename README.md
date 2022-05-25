56efc6046e4aff03032d588ba6ab16759e0e4f2a
moi <marco2391@gmail.com>
2022-05-25 14:41:27 -0400

    se creo archivo readme y se empieza a establecer los datos iniciales

---
+++ README.md
@@ -0,0 +1,1 @@
+Este es un programa para llevar un mejor control de las recaudaciones durante el turno de taxi.
--- taxi.py
+++ taxi.py
@@ -1,3 +1,14 @@
+from asyncore import read
+
+
 print("##########################")
 print("\t comienza el turno")
-print("##########################")
+print("########################## \n \n")
+
+
+movil= int(input("Ingresa el numero de movil: "))
+print(f"El movi usado para el servicio de hoy es el {movil} \n")
+
+
+kmsalida= int(input("ingresa el km de salida: "))
+print(f"se establecio el kilometraje de salida en: {kmsalida}km \n")

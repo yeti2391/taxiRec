6256fde5573699f389e4f0254f12d9224cf1a4e1
moi <marco2391@gmail.com>
2022-05-25 18:19:00 -0400

    en este punto falta agregar un condicional para q se termine el while al escribir -103

--- taxi.py
+++ taxi.py
@@ -1,5 +1,3 @@
-from asyncore import read
-
 
 print("##########################")
 print("\t comienza el turno")
@@ -7,8 +5,18 @@ print("########################## \n \n")
 
 
 movil= int(input("Ingresa el numero de movil: "))
-print(f"El movi usado para el servicio de hoy es el {movil} \n")
+print(f"El movil usado para el servicio de hoy es el {movil} \n")
 
 
 kmsalida= int(input("ingresa el km de salida: "))
 print(f"se establecio el kilometraje de salida en: {kmsalida}km \n")
+
+#### a partir de aca, recaudacion y subtotal
+importe=[0]
+print("ingresa el importe del viaje")
+while importe[-1] != -103:  
+    importe.append(int(input()))
+    #contar viajes:
+    print(f"Vas haciendo {len(importe)-1} viajes")
+    #total
+    print(sum(importe))
